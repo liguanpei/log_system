355772220b1af570b06dc9cee8206aa14a540aa0
liguanpei <guanpei.li@chinacache.com>
2014-09-09 18:03:56 +0800

    first commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,5 +7,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^index/$', 'mysite.views.index', {'template_name': 'index.html'}, name='index'),
+    url(r'^delete/$', 'mysite.views.delete', name='delete'),
+    url(r'^device_create', 'mysite.views.device_create', {'template_name': 'device_create.html'}, name='device_create'),
     url(r'^history_log/(?P<dev_name>.*)$', 'mysite.views.history_log', {'template_name': 'history_log.html'}, name='history_log'),
 )
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -6,6 +6,7 @@ import os
 
 from django.core.paginator import Paginator, EmptyPage
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render
@@ -16,6 +17,14 @@ def index(request, template_name):
     return render(request, template_name)
 
 def history_log(request, dev_name, template_name):
-    print dev_name
     return render(request, template_name)
+
+def delete(request):
+    return HttpResponseRedirect(reverse("index"))
     
+def device_create(request, template_name):
+    if request.method == 'POST':
+        device_name = request.POST.get("device_name")
+        device_area = request.POST.get("device_area")
+        print device_name, device_area
+    return render(request, template_name)
