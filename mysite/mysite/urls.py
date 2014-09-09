d0c1d0beb3f4c2ac522257f0cea904273e4bc39b
liguanpei <guanpei.li@chinacache.com>
2014-09-10 00:11:17 +0800

    添加模板以及基本结构

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,8 +6,12 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^index/$', 'mysite.views.index', {'template_name': 'index.html'}, name='index'),
-    url(r'^delete/$', 'mysite.views.delete', name='delete'),
+    url(r'^device_info/(?P<area_name>.*)$', 'mysite.views.device_info', {'template_name': 'device_info.html'}, name='device_info'),
     url(r'^device_create', 'mysite.views.device_create', {'template_name': 'device_create.html'}, name='device_create'),
+    url(r'^area_info/$', 'mysite.views.area_info', {'template_name': 'area_info.html'}, name='area_info'),
+    url(r'^area_create', 'mysite.views.area_create', {'template_name': 'area_create.html'}, name='area_create'),
+    url(r'^email_info/$', 'mysite.views.email_info', {'template_name': 'email_info.html'}, name='email_info'),
+    url(r'^email_create', 'mysite.views.email_create', {'template_name': 'email_create.html'}, name='email_create'),
     url(r'^history_log/(?P<dev_name>.*)$', 'mysite.views.history_log', {'template_name': 'history_log.html'}, name='history_log'),
+    url(r'^delete/$', 'mysite.views.delete', name='delete'),
 )
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -11,9 +11,36 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
-def index(request, template_name):
+def device_info(request, area_name, template_name):
     """
     """
+    return render(request, template_name)
+
+def device_create(request, template_name):
+    if request.method == 'POST':
+        device_name = request.POST.get("device_name")
+        device_area = request.POST.get("device_area")
+        print device_name, device_area
+    return render(request, template_name)
+
+def area_info(request, template_name):
+    """
+    """
+    return render(request, template_name)
+
+def area_create(request, template_name):
+    if request.method == 'POST':
+        pass
+    return render(request, template_name)
+
+def email_info(request, template_name):
+    """
+    """
+    return render(request, template_name)
+
+def email_create(request, template_name):
+    if request.method == 'POST':
+        pass
     return render(request, template_name)
 
 def history_log(request, dev_name, template_name):
@@ -22,9 +49,3 @@ def history_log(request, dev_name, template_name):
 def delete(request):
     return HttpResponseRedirect(reverse("index"))
     
-def device_create(request, template_name):
-    if request.method == 'POST':
-        device_name = request.POST.get("device_name")
-        device_area = request.POST.get("device_area")
-        print device_name, device_area
-    return render(request, template_name)
